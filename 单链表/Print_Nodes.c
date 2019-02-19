9de9f3776d3f5300b69516944e40a13dacb4861c
Adrian <40419358+EnthusiastLin@users.noreply.github.com>
2019-02-19 18:02:23 +0800

    Data Structure / Single Linked List with No Head

--- 单链表/Print_Nodes.c
+++ 单链表/Print_Nodes.c
@@ -4,14 +4,14 @@
 /* 打印结点  */
 void Print_Nodes( Node *root )
 {
-	Node *curr = NULL;  /* 保存 root 值  */
-	if ( NULL == root ) 
-		printf( "表中没有输入过数据!" );
+    Node *curr = NULL;  /* 保存 root 值  */
+    if ( NULL == root ) 
+	printf( "表中没有输入过数据!" );
 
-	curr = root;
-	while ( NULL != curr ) {
-		printf( " %d ", curr->data );
-		curr = curr->next; /* 将地址改成下一个  */
-	}
-	putchar( '\n' );
+    curr = root;
+    while ( NULL != curr ) {
+	printf( " %d ", curr->data );
+	curr = curr->next; /* 将地址改成下一个  */
+    }
+    putchar( '\n' );
 }
