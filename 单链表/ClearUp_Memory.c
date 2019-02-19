ff89ee66b58f2ed1c00e604d0d77170acfa1bebf
Adrian <40419358+EnthusiastLin@users.noreply.github.com>
2019-02-19 17:44:53 +0800

    Data Structure / Headless node single linked list

---
+++ 单链表/ClearUp_Memory.c
@@ -0,0 +1,13 @@
+
+#include "link_head.h"
+
+/* 清理内存  */
+void ClearUp_Memory( Node **pHead ) 
+{
+    Node *pSave = NULL;          /* 保存 *pHead 下个指向地址  */
+    while ( NULL != *pHead ) {
+	pSave = ( *pHead )->next; /* 将 *pHead 下个指向保存到 pSave 指针  */
+	free( *pHead );          /* 释放链表内存  */
+	*pHead = pSave;          /* 链接新的链表  */
+    }
+}
