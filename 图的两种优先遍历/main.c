5bc53e9c156a4eb278c75cecdc457c20a60c9f6b
Adrian <40419358+EnthusiastLin@users.noreply.github.com>
2019-02-21 19:36:38 +0800

    Data Structure / Graph of Traversal

--- 图的两种优先遍历/main.c
+++ 图的两种优先遍历/main.c
@@ -5,51 +5,51 @@
 
 int main( int argc, char **argv )
 {
-	Graph Gp = create_graph(); /* 创建图  */
+    Graph Gp = create_graph(); /* 创建图  */
 
-	/* 深度优先  */
-	printf( "对图进行深度优先遍历: \n" );
+    /* 深度优先  */
+    printf( "对图进行深度优先遍历: \n" );
 
-	printf( "从顶点A出发DFS的结果: " );
-	DFS_traverse( Gp, 0 );
-	printf( "\n" );
+    printf( "从顶点A出发DFS的结果: " );
+    DFS_traverse( Gp, 0 );
+    printf( "\n" );
 
-	printf( "从顶点H出发DFS的结果: " );
-	DFS_traverse( Gp, 7 );
-	printf( "\n" );
+    printf( "从顶点H出发DFS的结果: " );
+    DFS_traverse( Gp, 7 );
+    printf( "\n" );
 
-	printf( "从顶点E出发DFS的结果: " );
-	DFS_traverse( Gp, 4 );
-	puts( "\n" );
-
-
-	/* 广度优先  */
-	printf( "对图进行广度优先遍历: \n" );
-
-	printf( "从顶点A出发BFS的结果: " );
-	BFS_traverse( Gp, 0 );
-	printf( "\n" );
-
-	printf( "从顶点H出发BFS的结果: " );
-	BFS_traverse( Gp, 7 );
-	printf( "\n" );
-
-	printf( "从顶点E出发BFS的结果: " );
-	BFS_traverse( Gp, 4 );
-	printf( "\n" );
+    printf( "从顶点E出发DFS的结果: " );
+    DFS_traverse( Gp, 4 );
+    puts( "\n" );
 
 
-	/* 释放掉每个单链表所分配的内存  */
-	for ( int i = 0; VERTEXMAXSIZE > i; ++i ) {
-		free( Gp[ i ].first );
-		Gp[ i ].first = 0;    /* 防止悬垂指针产生  */
-	}
+    /* 广度优先  */
+    printf( "对图进行广度优先遍历: \n" );
+
+    printf( "从顶点A出发BFS的结果: " );
+    BFS_traverse( Gp, 0 );
+    printf( "\n" );
+
+    printf( "从顶点H出发BFS的结果: " );
+    BFS_traverse( Gp, 7 );
+    printf( "\n" );
+
+    printf( "从顶点E出发BFS的结果: " );
+    BFS_traverse( Gp, 4 );
+    printf( "\n" );
 
 
-	/* 释放掉顶点数组所分配的内存  */
-	free( Gp );
-	Gp = 0;
+    /* 释放掉每个单链表所分配的内存  */
+    for ( int i = 0; VERTEXMAXSIZE > i; ++i ) {
+	free( Gp[ i ].first );
+	Gp[ i ].first = 0;    /* 防止悬垂指针产生  */
+    }
 
-	system("pause");
-	return 0;
+
+    /* 释放掉顶点数组所分配的内存  */
+    free( Gp );
+    Gp = 0;
+
+    system("pause");
+    return 0;
 }
